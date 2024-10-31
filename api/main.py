785b66f6c83556a579aaa488080de43a80ac4d90
Jonathan Kwan <jonathankwan056@gmail.com>
2024-10-31 22:18:17 +0800

    fix: main

--- api/main.py
+++ api/main.py
@@ -1,25 +1,6 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-import joblib
-from sklearn.metrics.pairwise import cosine_similarity
-from fastapi.middleware.cors import CORSMiddleware
-
-# Data yang dikirimkan oleh user
-class UserInput(BaseModel):
-    skills: str
-    experience: int
-
-# Inisialisasi FastAPI
+from fastapi import FastAPI
 app = FastAPI()
 
-# Konfigurasi CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Mengizinkan semua asal
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 @app.get("/")
 async def homepage():
